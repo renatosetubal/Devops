8df22cde2442ab3f2815fcbf7a8b6c0c376d723b
Renato Miranda Setubal <remiset@gmail.com>
2024-03-20 19:52:47 -0300

    Configuraçãod e Ip manual via nmcli pelo script.

--- Lab01/scripts/aldb.sh
+++ Lab01/scripts/aldb.sh
@@ -8,7 +8,7 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'dba'@'%' WITH GRANT OPTION;FLU
 mysql Inova < /vagrant/Inova.sql
 #Atribuindo um ip fixo via linha de comando
 nmcli connection modify "System eth1" ipv4.addresses 10.0.19.65/24 ipv4.gateway 10.0.19.1 ipv4.method manual
-sudo nmcli connection down System eth1" && sudo nmcli connection up "System eth1"
+sudo nmcli connection down "System eth1" && sudo nmcli connection up "System eth1"
 
 
 
