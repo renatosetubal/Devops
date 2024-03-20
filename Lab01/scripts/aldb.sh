901ab9bf09a9bd19140034d5e30f4dd65ac6e5a9
Renato Miranda Setubal <remiset@gmail.com>
2024-03-20 19:41:53 -0300

    Máquina criando banco e importanto Sql para o banco

---
+++ Lab01/scripts/aldb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dnf update
+dnf install mariadb-server net-tools -y
+sudo systemctl enable --now mariadb
+mysql -u root -e "CREATE DATABASE Inova"
+mysql -u root -e "CREATE USER 'dba'@'%' IDENTIFIED BY 'dba';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'dba'@'%' WITH GRANT OPTION;FLUSH PRIVILEGES;"
+mysql Inova < /vagrant/Inova.sql
+#Atribuindo um ip fixo via linha de comando
+nmcli connection modify "System eth1" ipv4.addresses 10.0.19.65/24 ipv4.gateway 10.0.19.1 ipv4.method manual
+sudo nmcli connection down System eth1" && sudo nmcli connection up "System eth1"
+
+
+
